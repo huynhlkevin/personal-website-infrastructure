77f34fdd5e36c4ba75101169161883bdde7e1989
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-01 11:48:45 -0800

    add frontend automation resources

--- main.tf
+++ main.tf
@@ -295,3 +295,65 @@ resource "aws_lambda_function" "visitor" {
   handler       = "update_visitor_counter.lambda_handler"
   runtime       = "python3.13"
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"]
+}
+
+resource "aws_iam_role" "frontend_automation" {
+  name = "FrontendAutomation"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.github.arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:huynhlkevin/personal-website:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "frontend_automation" {
+  role = aws_iam_role.frontend_automation.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        "Resource" : "arn:aws:s3:::huynhlkevin.com/*"
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : "arn:aws:s3:::huynhlkevin.com"
+      }
+    ]
+  })
+}
