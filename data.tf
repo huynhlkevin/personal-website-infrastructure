bc7fac96b426607bb9e55626444d5a53eab90dc9
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-01 10:31:15 -0800

    add archive file for lambda code and replace rest api lambda_arn with reference

--- data.tf
+++ data.tf
@@ -3,7 +3,13 @@ data "template_file" "visitor_rest_api" {
 
   vars = {
     api_version                 = "1.0.0"
-    lambda_arn                  = "arn:aws:lambda:us-west-1:864899855377:function:addVisitor"
+    lambda_arn                  = aws_lambda_function.visitor.arn
     access_control_allow_origin = "https://www.${var.DOMAIN_NAME}"
   }
+}
+
+data "archive_file" "update_visitor_counter" {
+  type        = "zip"
+  source_file = "resources/lambda/update_visitor_counter.py"
+  output_path = "lambda_function_payload.zip"
 }
