2db2cb78fe7278633f18490316a50a3eed406d9a
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-01 20:46:05 -0800

    create website module

---
+++ modules/website/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  description = "Domain name for the website"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Domain zone id"
+  type        = string
+}
