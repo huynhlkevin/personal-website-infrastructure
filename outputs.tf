b6efb1de48d64d750ce5a0d3ab5511716ee4bcdc
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-02 23:57:46 -0800

    fix errors

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,11 @@ output "cloudfront_domain_name" {
 
 output "rest_api_invoke_url" {
   description = "REST API invoke url"
-  value       = module.visitor_counter_backend.rest_api.invoke.url
+  value       = module.visitor_counter_backend.rest_api.invoke_url
 }
 
 output "rest_api_key" {
   description = "REST API key"
   value       = module.visitor_counter_backend.rest_api.api_key
+  sensitive   = true
 }
