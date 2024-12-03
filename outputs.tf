0a68e77cb62224a456a222b7a2c26bef13537061
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-02 23:52:13 -0800

    split rest api into separate outputs

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,12 @@ output "cloudfront_domain_name" {
   value       = module.website.cloudfront_domain_name
 }
 
-output "rest_api" {
-  description = "REST API information"
-  value       = module.visitor_counter_backend.rest_api
-  sensitive   = true
+output "rest_api_invoke_url" {
+  description = "REST API invoke url"
+  value       = module.visitor_counter_backend.rest_api.invoke.url
+}
+
+output "rest_api_key" {
+  description = "REST API key"
+  value       = module.visitor_counter_backend.rest_api.api_key
 }
