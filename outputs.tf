76823016de8dab3b67c90ac4baa3d0f70dc15d68
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-02 23:29:45 -0800

    add info needed for frontend

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "cloudfront_domain_name" {
+  description = "Cloudfront domain name"
+  value       = module.website.cloudfront_domain_name
+}
+
+output "rest_api" {
+  description = "REST API information"
+  value       = module.visitor_counter_backend.rest_api
+}
