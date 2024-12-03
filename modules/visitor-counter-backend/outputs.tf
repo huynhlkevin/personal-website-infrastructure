7c8aa37936c9eabac87c48bdebcce319b5673c02
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-03 00:00:04 -0800

    fix name

--- modules/visitor-counter-backend/outputs.tf
+++ modules/visitor-counter-backend/outputs.tf
@@ -1,7 +1,7 @@
 output "rest_api" {
   description = "REST API information"
   value = {
-    invocation_url = aws_api_gateway_stage.visitor.invoke_url
+    invoke_url = aws_api_gateway_stage.visitor.invoke_url
     api_key        = aws_api_gateway_api_key.visitor.value
   }
 }
