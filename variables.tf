907b04969e193af1967e75d63cc89beb370d7c49
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-04 00:28:21 -0800

    Fix CORS issues for development deployments (#19)

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "CLOUDFLARE_ZONE_ID" {
   description = "Cloudflare domain zone id"
   type        = string
+  default     = ""
 }
 
 variable "DOMAIN_NAME" {
   description = "Domain name"
   type        = string
+  default     = ""
 }
