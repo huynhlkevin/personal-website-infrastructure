3d1d0cd9ee855b89f5f73a3a19ea6440dbd96953
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-04 20:43:20 -0800

    Add website_url output to deploy development workflow (#22)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,13 @@
 output "cloudfront_domain_name" {
   description = "Cloudfront domain name"
   value       = module.website.cloudfront_domain_name
+  sensitive   = true
 }
 
 output "rest_api_invoke_url" {
   description = "REST API invoke url"
   value       = module.visitor_counter_backend.rest_api.invoke_url
+  sensitive   = true
 }
 
 output "rest_api_key" {
@@ -17,4 +19,5 @@ output "rest_api_key" {
 output "bucket_name" {
   description = "S3 bucket name"
   value       = module.website.bucket_id
+  sensitive   = true
 }
