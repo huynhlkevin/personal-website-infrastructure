ae6e1d511c9a3989c1f18b0ed689ae3a282a2a29
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2025-01-08 23:12:39 -0800

    remove sensitive parameter

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,24 @@
 output "cloudfront_domain_name" {
   description = "Cloudfront domain name"
   value       = module.website.cloudfront_domain_name
-  sensitive   = true
 }
 
 output "rest_api_invoke_url" {
   description = "REST API invoke url"
   value       = module.visitor_counter_backend.rest_api.invoke_url
-  sensitive   = true
 }
 
 output "rest_api_key" {
   description = "REST API key"
   value       = module.visitor_counter_backend.rest_api.api_key
-  sensitive   = true
 }
 
 output "bucket_name" {
   description = "S3 bucket name"
   value       = module.website.bucket_id
-  sensitive   = true
 }
 
 output "frontend_automation_aws_role" {
   description = "AWS role arn that can be used in GitHub Actions automation"
   value       = module.frontend_automation.role_arn
-  sensitive   = true
 }
